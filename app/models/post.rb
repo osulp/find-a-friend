a4bb7887e27178645b19dc447178c7e8bb1c7d15
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-08-29 10:54:44 -0700

    made meeting time and end time required fields

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, :onid, :description, :location, presence: true
+  validates :title, :onid, :description, :location, :meeting_time, :end_time, presence: true
 
   belongs_to :user
 
