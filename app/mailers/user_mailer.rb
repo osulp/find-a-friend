f1e8f638c1ffbda7ab2c53de8110a5687b82f7dd
balesh2 <balesh@onid.oregonstate.edu>
2014-09-08 10:19:43 -0700

    updates mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "findafriendapp@gmail.com"
-	
-  #resolved merge
+  
 	def new_post_email(post)
 		@post = post
     if @post.allow_onid
-		  mail(to: @post.recipients, subject: @post.title, reply_to: "#{@post.onid}@onid.oregonstate.edu")
+      mail(to: @post.recipients, subject: @post.title, reply_to: "#{@post.onid}@onid.oregonstate.edu")
     else
       mail(to: @post.recipients, subject: @post.title)
     end
