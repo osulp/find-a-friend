6ecfaa7bc584c01f14748b72d2109e131dfa164a
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-09-22 13:22:54 -0700

    Added description to popover and location class

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918171530) do
+ActiveRecord::Schema.define(version: 20140922183001) do
 
   create_table "admins", force: true do |t|
     t.string   "onid"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140918171530) do
     t.datetime "updated_at"
     t.integer  "post_id"
     t.string   "photo"
+    t.text     "description"
   end
 
   add_index "locations", ["post_id"], name: "index_locations_on_post_id"
